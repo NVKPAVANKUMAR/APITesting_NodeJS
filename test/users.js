259a8f39a56dcbba726dc5b62b907c6914aadb84
PAVAN KUMAR <npavankumar144@gmail.com>
2020-10-12 12:40:36 +0530

    first commit on api testing

--- test/users.js
+++ test/users.js
@@ -1,6 +1,5 @@
 import supertest from 'supertest';
 const request = supertest('https://gorest.co.in/public-api/');
-
 import { expect } from 'chai';
 
 const TOKEN =
@@ -19,13 +18,16 @@ describe('Users', () => {
   });
 
   it('GET /users/:id', () => {
-    return request.get(`users/1?access-token=${TOKEN}`).then((res) => {
-      expect(res.body.data.id).to.be.eq(1);
+    return request.get(`users/123?access-token=${TOKEN}`).then((res) => {
+      expect(res.body.data.id).to.be.eq(123);
     });
   });
 
   it('GET /users with query params', () => {
-    const url = `users?access-token=${TOKEN}&page=5&gender=Female&status=Active`;
+    const url = `users?access-token=${TOKEN}&
+    page=5&
+    gender=Female&
+    status=Active`;
 
     return request.get(url).then((res) => {
       expect(res.body.data).to.not.be.empty;
@@ -71,7 +73,7 @@ describe('Users', () => {
 
   it('DELETE /users/:id', () => {
     return request
-      .delete('users/21')
+      .delete('users/123')
       .set('Authorization', `Bearer ${TOKEN}`)
       .then((res) => {
         console.log(res.body);
